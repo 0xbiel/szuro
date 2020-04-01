c98a6561109b56976ea0e03092c502828d7c8b13
Gabriel <bielp07@gmail.com>
2020-04-01 10:07:03 -0300

    Core app done. Maybe add args support later so you don't have to change the source code everytime.

--- szuro.py
+++ szuro.py
@@ -1,17 +1,30 @@
 import frida
 
-session = frida.get_usb_device().attach('org.mozilla.firefox') #replace with the app you are hacking.
+session = frida.get_usb_device().attach('org.mozilla.firefox')  #replace with the app you are hacking.
 
-android_view_methods[] #store methods in an array
+android_view_methods = []
 
 source = """
-
-Java.perform(function() {
+Java.perform(function () {
     var v = Java.use("android.view.View");
     var m = v.class.getMethods();
-    for var i = 0; i < m.length; i++) {
+    for(var i = 0; i < m.length; i++) {
         send(m[i].toString());
     }
 });
-
 """
+
+script = session.create_script(source)
+
+def on_message(message, data):
+    if "Text" in message['payload']: #replace "Text" with what you want to filter
+        android_view_methods.append(message['payload'])
+
+script.on('message', on_message)
+
+script.load()
+
+for method in android_view_methods:
+    print(method)
+
+session.detach()
