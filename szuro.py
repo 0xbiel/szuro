1a4564549139bb5cd94c49e048d35db09883a689
Gabriel <bielp07@gmail.com>
2020-04-14 11:11:17 -0300

    Prompt package ID and what the user wanna filter.

--- szuro.py
+++ szuro.py
@@ -1,6 +1,19 @@
 import frida
 
-session = frida.get_usb_device().attach('org.mozilla.firefox')  #replace with the app you are hacking.
+print("""
+____ ___  _  _ ____ ____ 
+[__    /  |  | |__/ |  | 
+___]  /__ |__| |  \ |__| 
+                                 
+""")
+
+appid = input("Package ID: ")
+
+mf = input("What do you want to filter? ")
+
+print("\n")
+
+session = frida.get_usb_device().attach(appid)
 
 android_view_methods = []
 
@@ -17,7 +30,7 @@ Java.perform(function () {
 script = session.create_script(source)
 
 def on_message(message, data):
-    if "Text" in message['payload']: #replace "Text" with what you want to filter
+    if mf in message['payload']: #replace "Text" with what you want to filter
         android_view_methods.append(message['payload'])
 
 script.on('message', on_message)
